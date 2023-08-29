f8b85afc7846c17e952e175f570586241504eede
Sergei Lukianov <me@slukjanov.name>
2023-08-29 14:45:06 -0700

    Initial scaffolding using kubebuilder

---
+++ api/fabricator/v1alpha2/fabricator_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2023 Hedgehog.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha2
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// FabricatorSpec defines the desired state of Fabricator
+type FabricatorSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Fabricator. Edit fabricator_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// FabricatorStatus defines the observed state of Fabricator
+type FabricatorStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Fabricator is the Schema for the fabricators API
+type Fabricator struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   FabricatorSpec   `json:"spec,omitempty"`
+	Status FabricatorStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// FabricatorList contains a list of Fabricator
+type FabricatorList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Fabricator `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Fabricator{}, &FabricatorList{})
+}
