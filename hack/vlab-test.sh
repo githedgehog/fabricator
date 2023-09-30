6617f0fdbeddd917badf3b11f7e76ece4dbab86d
Sergei Lukianov <me@slukjanov.name>
2023-09-29 23:38:09 -0700

    Enable testing creation of VPC peering in CI

--- hack/vlab-test.sh
+++ hack/vlab-test.sh
@@ -6,8 +6,13 @@ function control {
     bin/hhfab vlab ssh --vm control-1 PATH=/opt/bin "$@"
 }
 
-control kubectl fabric vpc create --name vpc-1 --subnet 10.90.0.1/24
+control kubectl fabric vpc create --name vpc-1 --subnet 10.90.1.1/24
+control kubectl fabric vpc create --name vpc-2 --subnet 10.90.2.1/24
+
 control kubectl fabric vpc attach --vpc vpc-1 --conn server-1--mclag--switch-1--switch-2
-control kubectl fabric vpc attach --vpc vpc-1 --conn server-2--mclag--switch-1--switch-2
+control kubectl fabric vpc attach --vpc vpc-2 --conn server-2--mclag--switch-1--switch-2
+
+control kubectl fabric vpc peer --vpc vpc-1 --vpc vpc-2
 
 control kubectl get agent -o wide
+control kubectl wait --for=condition=Applied agent/switch-1 agent/switch-2 --timeout=600s
