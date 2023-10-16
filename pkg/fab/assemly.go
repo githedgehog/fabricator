39cb56a992465e823af5a2dcd3fed2def4fa848e
Sergei Lukianov <me@slukjanov.name>
2023-10-15 21:55:30 -0700

    Bump fabric to v0.18.2

--- pkg/fab/assemly.go
+++ pkg/fab/assemly.go
@@ -76,7 +76,7 @@ var (
 	}
 
 	// Fabric
-	REF_FABRIC_VERSION           = cnc.Ref{Tag: "v0.18.1"}
+	REF_FABRIC_VERSION           = cnc.Ref{Tag: "v0.18.2"}
 	REF_FABRIC_API_CHART         = cnc.Ref{Name: "fabric/charts/fabric-api"}
 	REF_FABRIC_CHART             = cnc.Ref{Name: "fabric/charts/fabric"}
 	REF_FABRIC_IMAGE             = cnc.Ref{Name: "fabric/fabric"}
