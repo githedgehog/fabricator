4a21c08ab24699bfcfe7825ec6fc183df32548ae
Sergei Lukianov <me@slukjanov.name>
2023-11-13 16:51:06 -0800

    Make hydrate support all loopbacks and handle mclag

--- pkg/fab/wiring/hydrate.go
+++ pkg/fab/wiring/hydrate.go
@@ -64,10 +64,13 @@ const (
 	SPINE_OFFSET = 200
 	LEAF_OFFSET  = 100
 
-	CONTROL_IP_NET       = 2
-	SWITCH_IP_NET        = 10
+	// 1 is reserved for the control VIP
 	MCLAG_SESSION_IP_NET = 5
-	FABRIC_IP_NET        = 20 // can take more than one /24, let's book 10
+	SWITCH_IP_NET        = 10
+	PROTOCOL_IP_NET      = 11
+	VTEP_IP_NET          = 12
+	CONTROL_IP_NET       = 20 // single /24 is more than enough
+	FABRIC_IP_NET        = 30 // can take more than one /24, let's book 10
 )
 
 func Hydrate(data *wiring.Data, cfg *HydrateConfig) error {
@@ -76,18 +79,57 @@ func Hydrate(data *wiring.Data, cfg *HydrateConfig) error {
 	}
 	cfg.Subnet = strings.TrimSuffix(cfg.Subnet, ".0.0/16")
 
+	// make sure we don't have any leftover data
+	for _, sw := range data.Switch.All() {
+		sw.Spec.ASN = 0
+		sw.Spec.IP = ""
+		sw.Spec.VTEPIP = ""
+		sw.Spec.ProtocolIP = ""
+	}
+
+	mclagPeer := map[string]string{}
+	for _, conn := range data.Connection.All() {
+		if conn.Spec.MCLAGDomain == nil {
+			continue
+		}
+
+		sws, _, _, err := conn.Spec.Endpoints()
+		if err != nil {
+			return errors.Wrapf(err, "error getting endpoints for MCLAG domain connection %s", conn.Name)
+		}
+		if len(sws) != 2 {
+			return errors.Errorf("MCLAG domain connection %s has %d endpoints, expected 2", conn.Name, len(sws))
+		}
+
+		mclagPeer[sws[0]] = sws[1]
+		mclagPeer[sws[1]] = sws[0]
+	}
+
 	spine := 0
 	leaf := 0
 	for _, sw := range data.Switch.All() {
 		if sw.Spec.Role.IsSpine() {
 			sw.Spec.ASN = cfg.SpineASN
 			sw.Spec.IP = fmt.Sprintf("%s.%d.%d/32", cfg.Subnet, SWITCH_IP_NET, spine+SPINE_OFFSET)
+			sw.Spec.ProtocolIP = fmt.Sprintf("%s.%d.%d/32", cfg.Subnet, PROTOCOL_IP_NET, spine+SPINE_OFFSET)
 
 			spine++
 		}
 		if sw.Spec.Role.IsLeaf() {
 			sw.Spec.ASN = cfg.LeafASNStart + uint32(leaf)
 			sw.Spec.IP = fmt.Sprintf("%s.%d.%d/32", cfg.Subnet, SWITCH_IP_NET, leaf+LEAF_OFFSET)
+			sw.Spec.ProtocolIP = fmt.Sprintf("%s.%d.%d/32", cfg.Subnet, PROTOCOL_IP_NET, leaf+LEAF_OFFSET)
+			sw.Spec.VTEPIP = fmt.Sprintf("%s.%d.%d/32", cfg.Subnet, VTEP_IP_NET, leaf+LEAF_OFFSET)
+
+			// MCLAG pair should have the same ASN and VTEP IP
+			if peer, ok := mclagPeer[sw.Name]; ok {
+				if peerSw := data.Switch.Get(peer); peerSw != nil {
+					if peerSw.Spec.ASN != 0 && peerSw.Spec.VTEPIP != "" {
+						sw.Spec.ASN = peerSw.Spec.ASN
+						sw.Spec.VTEPIP = peerSw.Spec.VTEPIP
+					}
+				}
+			}
 
 			leaf++
 		}
