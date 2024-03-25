11b5233c3a0b3de579477dbcfe1d9648c6173fd6
Sergei Lukianov <me@slukjanov.name>
2024-03-25 16:33:11 -0700

    Bump fabric artifacts to v0.34.1

--- pkg/fab/assembly.go
+++ pkg/fab/assembly.go
@@ -155,7 +155,7 @@ var (
 	}
 
 	// Fabric
-	RefFabricVersion         = cnc.Ref{Tag: "v0.33.0"}
+	RefFabricVersion         = cnc.Ref{Tag: "v0.34.1"}
 	RefFabricAPIChart        = cnc.Ref{Name: "fabric/charts/fabric-api"}
 	RefFabricChart           = cnc.Ref{Name: "fabric/charts/fabric"}
 	RefFabricImage           = cnc.Ref{Name: "fabric/fabric"}
