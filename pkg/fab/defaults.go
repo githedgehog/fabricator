b2136cdf94130a55e6b1cf5130bdae23fe67a7dd
Sergei Lukianov <me@slukjanov.name>
2024-09-19 20:09:42 -0700

    Update defaults with missing values

--- pkg/fab/defaults.go
+++ pkg/fab/defaults.go
@@ -5,11 +5,12 @@ import (
 	fabapi "go.githedgehog.com/fabricator/api/fabricator/v1beta1"
 )
 
+// IPs for ~1000 switches, ASNs are for 433 leafs
 var DefaultConfig = fabapi.FabConfig{
 	Control: fabapi.ControlConfig{
-		ManagementSubnet:  "172.30.1.0/24",
-		VIP:               "172.30.1.1",
-		TLSSAN:            []string{}, // TODO
+		ManagementSubnet:  "172.30.0.0/21", // 2046 hosts: 172.30.0.1 - 172.30.7.254
+		VIP:               "172.30.0.1",
+		TLSSAN:            []string{},
 		KubeClusterSubnet: "172.28.0.0/16",
 		KubeServiceSubnet: "172.29.0.0/16",
 		KubeClusterDNS:    "172.29.0.10",
@@ -17,18 +18,25 @@ var DefaultConfig = fabapi.FabConfig{
 	},
 	Registry: fabapi.RegistryConfig{},
 	Fabric: fabapi.FabricConfig{
-		SpineASN:            65100,
+		Mode:                fmeta.FabricModeSpineLeaf,
+		ManagementDHCPStart: "172.30.4.0", // second half of the management subnet
+		SpineASN:            65100,        // TODO probably switch to 32-bit ASNs
 		LeafASNStart:        65101,
-		LeafASNEnd:          65999,
-		ProtocolSubnet:      "TBD",               // TODO
-		VTEPSubnet:          "TBD",               // TODO
-		FabricSubnet:        "TBD",               // TODO
-		BaseVPCCommunity:    "50000:0",           // TODO make sure it's really used
-		VPCIRBVLANs:         []fmeta.VLANRange{}, // TODO
-		VPCWorkaroundVLANs:  []fmeta.VLANRange{}, // TODO
-		VPCWorkaroundSubnet: "172.30.240.0/20",   // TODO make sure it's really used
+		LeafASNEnd:          65534,             // only 433 leafs
+		ProtocolSubnet:      "172.30.8.0/21",   // 1022 hosts: 172.30.8.1 - 172.30.11.254
+		VTEPSubnet:          "172.30.12.0/22",  // 1022 hosts: 172.30.12.1 - 172.30.15.254
+		FabricSubnet:        "172.30.128.0/17", // 16384 /31 subnets: 172.30.128.1 - 172.30.255.254
+		BaseVPCCommunity:    "50000:0",         // TODO make sure it's really used
+		VPCIRBVLANs: []fmeta.VLANRange{
+			{From: 3000, To: 3999},
+		},
+		VPCWorkaroundVLANs: []fmeta.VLANRange{
+			{From: 100, To: 3999},
+		},
+		VPCWorkaroundSubnet: "172.30.112.0/19",   // 4096 /31 subnets: 172.30.96.1 - 172.30.127.254 // TODO make sure it's really used
 		ESLAGMACBase:        "f2:00:00:00:00:00", // TODO make sure it's really used
 		ESLAGESIPrefix:      "00:f2:00:00:",      // TODO make sure it's really used
 		DefaultSwitchUsers:  map[string]fabapi.SwitchUser{},
+		DefaultAlloyConfig:  fmeta.AlloyConfig{},
 	},
 }
