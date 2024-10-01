e1cf364b09790a1d1f38f5883f620383d7606b0c
Sergei Lukianov <me@slukjanov.name>
2024-09-30 22:11:04 -0700

    Actually ungz recipe bin before use

--- pkg/embed/recipebin/recipebin.go
+++ pkg/embed/recipebin/recipebin.go
@@ -2,11 +2,13 @@ package recipebin
 
 import (
 	_ "embed"
+
+	"go.githedgehog.com/fabricator/pkg/embed"
 )
 
-//go:embed hhfab-recipe
-var data []byte
+//go:embed hhfab-recipe.gz
+var compressed []byte
 
-func Bytes() []byte {
-	return data
+func Bytes() ([]byte, error) {
+	return embed.Bytes(compressed) //nolint:wrapcheck
 }
