9bd6aeba3c5b8f085b2134f612a35e2303e03d45
Sergei Lukianov <me@slukjanov.name>
2024-10-02 15:28:02 -0700

    Make sure dhcp is running and install mgmt subnet config

--- pkg/fab/comp/fabric/fabric.go
+++ pkg/fab/comp/fabric/fabric.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/netip"
 
+	dhcpapi "go.githedgehog.com/fabric/api/dhcp/v1alpha2"
 	"go.githedgehog.com/fabric/api/meta"
 	fabapi "go.githedgehog.com/fabricator/api/fabricator/v1beta1"
 	"go.githedgehog.com/fabricator/pkg/fab/comp"
@@ -41,93 +42,113 @@ var dhcpValuesTmpl string
 //go:embed proxy_values.tmpl.yaml
 var proxyValuesTmpl string
 
-var _ comp.KubeInstall = Install
+func Install(control fabapi.ControlNode) comp.KubeInstall {
+	return func(cfg fabapi.Fabricator) ([]client.Object, error) {
+		fabricCfg, err := GetFabricConfig(cfg)
+		if err != nil {
+			return nil, fmt.Errorf("getting fabric config: %w", err)
+		}
 
-func Install(cfg fabapi.Fabricator) ([]client.Object, error) {
-	fabricCfg, err := GetFabricConfig(cfg)
-	if err != nil {
-		return nil, fmt.Errorf("getting fabric config: %w", err)
-	}
+		fabricCfgYaml, err := yaml.Marshal(fabricCfg)
+		if err != nil {
+			return nil, fmt.Errorf("marshalling fabric config: %w", err)
+		}
 
-	fabricCfgYaml, err := yaml.Marshal(fabricCfg)
-	if err != nil {
-		return nil, fmt.Errorf("marshalling fabric config: %w", err)
-	}
+		apiHelm, err := comp.NewHelmChart(cfg, "fabric-api", APIChartRef,
+			string(cfg.Status.Versions.Fabric.API), "", true, "")
+		if err != nil {
+			return nil, fmt.Errorf("creating fabric API helm chart: %w", err)
+		}
 
-	apiHelm, err := comp.NewHelmChart(cfg, "fabric-api", APIChartRef,
-		string(cfg.Status.Versions.Fabric.API), "", true, "")
-	if err != nil {
-		return nil, fmt.Errorf("creating fabric API helm chart: %w", err)
-	}
+		ctrlRef, err := comp.ImageURL(cfg, CtrlRef)
+		if err != nil {
+			return nil, fmt.Errorf("getting image URL for %q: %w", CtrlRef, err)
+		}
+		ctlrValues, err := tmplutil.FromTemplate("ctrl-values", ctrlValuesTmpl, map[string]any{
+			"Repo": ctrlRef,
+			"Tag":  string(cfg.Status.Versions.Fabric.Controller),
+		})
+		if err != nil {
+			return nil, fmt.Errorf("ctrl values: %w", err)
+		}
+		ctrlHelm, err := comp.NewHelmChart(cfg, "fabric", CtrlChartRef,
+			string(cfg.Status.Versions.Fabric.Controller), "", false, ctlrValues)
+		if err != nil {
+			return nil, fmt.Errorf("creating fabric helm chart: %w", err)
+		}
 
-	ctrlRef, err := comp.ImageURL(cfg, CtrlRef)
-	if err != nil {
-		return nil, fmt.Errorf("getting image URL for %q: %w", CtrlRef, err)
-	}
-	ctlrValues, err := tmplutil.FromTemplate("ctrl-values", ctrlValuesTmpl, map[string]any{
-		"Repo": ctrlRef,
-		"Tag":  string(cfg.Status.Versions.Fabric.Controller),
-	})
-	if err != nil {
-		return nil, fmt.Errorf("ctrl values: %w", err)
-	}
-	ctrlHelm, err := comp.NewHelmChart(cfg, "fabric", CtrlChartRef,
-		string(cfg.Status.Versions.Fabric.Controller), "", false, ctlrValues)
-	if err != nil {
-		return nil, fmt.Errorf("creating fabric helm chart: %w", err)
-	}
+		dhcpRef, err := comp.ImageURL(cfg, DHCPRef)
+		if err != nil {
+			return nil, fmt.Errorf("getting image URL for %q: %w", DHCPRef, err)
+		}
 
-	dhcpRef, err := comp.ImageURL(cfg, DHCPRef)
+		dhcpValues, err := tmplutil.FromTemplate("dhcp-values", dhcpValuesTmpl, map[string]any{
+			"Repo":            dhcpRef,
+			"Tag":             string(cfg.Status.Versions.Fabric.DHCPD),
+			"ListenInterface": control.Spec.Management.Interface,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("dhcp values: %w", err)
+		}
+		dhcpHelm, err := comp.NewHelmChart(cfg, "fabric-dhcpd", DHCPChartRef,
+			string(cfg.Status.Versions.Fabric.DHCPD), "", false, dhcpValues)
+		if err != nil {
+			return nil, fmt.Errorf("creating fabric DHCP helm chart: %w", err)
+		}
+
+		proxyRef, err := comp.ImageURL(cfg, ProxyRef)
+		if err != nil {
+			return nil, fmt.Errorf("getting image URL for %q: %w", ProxyRef, err)
+		}
+		proxyValues, err := tmplutil.FromTemplate("proxy-values", proxyValuesTmpl, map[string]any{
+			"Repo": proxyRef,
+			"Tag":  string(cfg.Status.Versions.Fabric.Proxy),
+			"Port": ProxyNodePort,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("proxy values: %w", err)
+		}
+		proxyHelm, err := comp.NewHelmChart(cfg, "fabric-proxy", ProxyChartRef,
+			string(cfg.Status.Versions.Fabric.ProxyChart), "", false, proxyValues)
+		if err != nil {
+			return nil, fmt.Errorf("creating fabric proxy helm chart: %w", err)
+		}
+
+		// TODO boot
+
+		return []client.Object{
+			apiHelm,
+			comp.NewConfigMap("fabric-config", map[string]string{
+				"config.yaml": string(fabricCfgYaml),
+			}),
+			ctrlHelm,
+			dhcpHelm,
+			proxyHelm,
+		}, nil
+	}
+}
+
+// TODO move to fabricator
+func InstallManagementDHCPSubnet(cfg fabapi.Fabricator) ([]client.Object, error) {
+	mgmt, err := cfg.Spec.Config.Control.ManagementSubnet.Parse()
 	if err != nil {
-		return nil, fmt.Errorf("getting image URL for %q: %w", DHCPRef, err)
+		return nil, fmt.Errorf("parsing management subnet: %w", err)
 	}
 
 	controlVIP, err := cfg.Spec.Config.Control.VIP.Parse()
 	if err != nil {
 		return nil, fmt.Errorf("parsing control VIP: %w", err)
 	}
-	dhcpValues, err := tmplutil.FromTemplate("dhcp-values", dhcpValuesTmpl, map[string]any{
-		"Repo":          dhcpRef,
-		"Tag":           string(cfg.Status.Versions.Fabric.DHCPD),
-		"ListenAddress": controlVIP.Addr().String(),
-	})
-	if err != nil {
-		return nil, fmt.Errorf("dhcp values: %w", err)
-	}
-	dhcpHelm, err := comp.NewHelmChart(cfg, "fabric-dhcpd", DHCPChartRef,
-		string(cfg.Status.Versions.Fabric.DHCPD), "", false, dhcpValues)
-	if err != nil {
-		return nil, fmt.Errorf("creating fabric DHCP helm chart: %w", err)
-	}
-
-	proxyRef, err := comp.ImageURL(cfg, ProxyRef)
-	if err != nil {
-		return nil, fmt.Errorf("getting image URL for %q: %w", ProxyRef, err)
-	}
-	proxyValues, err := tmplutil.FromTemplate("proxy-values", proxyValuesTmpl, map[string]any{
-		"Repo": proxyRef,
-		"Tag":  string(cfg.Status.Versions.Fabric.Proxy),
-		"Port": ProxyNodePort,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("proxy values: %w", err)
-	}
-	proxyHelm, err := comp.NewHelmChart(cfg, "fabric-proxy", ProxyChartRef,
-		string(cfg.Status.Versions.Fabric.ProxyChart), "", false, proxyValues)
-	if err != nil {
-		return nil, fmt.Errorf("creating fabric proxy helm chart: %w", err)
-	}
-
-	// TODO boot
 
 	return []client.Object{
-		apiHelm,
-		comp.NewConfigMap("fabric-config", map[string]string{
-			"config.yaml": string(fabricCfgYaml),
+		comp.NewDHCPSubnet(dhcpapi.ManagementSubnet, dhcpapi.DHCPSubnetSpec{
+			Subnet:    dhcpapi.ManagementSubnet,
+			CIDRBlock: mgmt.Masked().String(),
+			Gateway:   mgmt.Addr().Next().String(),
+			StartIP:   string(cfg.Spec.Config.Fabric.ManagementDHCPStart),
+			// TODO endIP?
+			DefaultURL: "http://" + controlVIP.Addr().String() + ":32000/onie", // TODO const
 		}),
-		ctrlHelm,
-		dhcpHelm,
-		proxyHelm,
 	}, nil
 }
 
