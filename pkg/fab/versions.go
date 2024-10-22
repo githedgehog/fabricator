5f723d9b07c75b7cf6691568b0ad16c641fd1836
Sergei Lukianov <me@slukjanov.name>
2024-10-22 15:26:58 -0700

    Bump fabric to v0.53.1

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -13,7 +13,7 @@ import (
 
 var (
 	FabricatorVersion = meta.Version(version.Version)
-	FabricVersion     = meta.Version("v0.52.5")
+	FabricVersion     = meta.Version("v0.53.1")
 )
 
 var Versions = fabapi.Versions{
