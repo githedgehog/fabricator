09f7ececa2c15448f5b266656faaae5ec3a27a76
Sergei Lukianov <me@slukjanov.name>
2025-01-29 18:57:44 -0800

    Bump fabric to v0.61.1
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -13,7 +13,7 @@ import (
 
 var (
 	FabricatorVersion = meta.Version(version.Version)
-	FabricVersion     = meta.Version("v0.61.0")
+	FabricVersion     = meta.Version("v0.61.1")
 )
 
 var Versions = fabapi.Versions{
