71dd418765adae0709cb797e1d5b864d93e2e3a7
Sergei Lukianov <me@slukjanov.name>
2025-03-25 23:40:12 -0700

    fix: make inspect lldp/bgp work fine with gateways
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -13,7 +13,7 @@ import (
 
 var (
 	FabricatorVersion = meta.Version(version.Version)
-	FabricVersion     = meta.Version("v0.71.0")
+	FabricVersion     = meta.Version("v0.71.1")
 )
 
 var Versions = fabapi.Versions{
