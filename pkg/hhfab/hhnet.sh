3c3d8aef88583ccd4091f38ffb746de54fb55836
Emanuele Di Pascale <emanuele@githedgehog.com>
2025-04-16 13:17:32 +0200

    chore: accept hash policy in hhnet bond
    and update the calls from fabricator to add the new "default"
    of layer2+3, as opposed to the previous implicit layer2.
    
    Part of #549
    
    Signed-off-by: Emanuele Di Pascale <emanuele@githedgehog.com>

--- pkg/hhfab/hhnet.sh
+++ pkg/hhfab/hhnet.sh
@@ -22,10 +22,11 @@ function cleanup() {
 
 function setup_bond() {
     local bond_name=$1
+    local hash_policy=$2
 
-    sudo ip l a "$bond_name" type bond miimon 100 mode 802.3ad
+    sudo ip l a "$bond_name" type bond miimon 100 mode 802.3ad xmit_hash_policy "$hash_policy"
 
-    for iface in "${@:2}"; do
+    for iface in "${@:3}"; do
         # cannot enslave interface if it is up
         sudo ip l s "$iface" down 2> /dev/null || true
         sudo ip l s "$iface" master "$bond_name"
@@ -64,7 +65,7 @@ function get_ip() {
 
 # Usage:
 # hhnet cleanup
-# hhnet bond 1000 enp2s1 enp2s2 enp2s3 enp2s4
+# hhnet bond 1000 layer2+3 enp2s1 enp2s2 enp2s3 enp2s4
 # hhnet vlan 1000 enp2s1
 
 function usage() {
@@ -72,7 +73,7 @@ function usage() {
     echo " Cleanup all interfaces (enp2s1-9, bond0-3, vlans 1000-1020): " >&2
     echo "  hhnet cleanup" >&2
     echo " Setup bond from provided interfaces (at least one) and vlan on top of it" >&2
-    echo "  hhnet bond 1000 enp2s1 enp2s2 enp2s3 enp2s4" >&2
+    echo "  hhnet bond 1000 layer2+3 enp2s1 enp2s2 enp2s3 enp2s4" >&2
     echo " Setup vlan on top of provided interface (exactly one)" >&2
     echo "  hhnet vlan 1000 enp2s1" >&2
 }
@@ -86,8 +87,8 @@ elif [ "$1" == "cleanup" ]; then
 
     exit 0
 elif [ "$1" == "bond" ]; then
-    if [ "$#" -lt 3 ]; then
-        echo "Usage: $0 bond <vlan_id> <iface1> [<iface2> ...]" >&2
+    if [ "$#" -lt 4 ]; then
+        echo "Usage: $0 bond <vlan_id> <hash_policy> <iface1> [<iface2> ...]" >&2
         exit 1
     fi
 
