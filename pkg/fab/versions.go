3a817b64dc0e74730cd6052927e55a81ac9754c7
Frostman <134872+Frostman@users.noreply.github.com>
2025-05-06 00:51:17 +0000

    bump: gateway to v0.4.1
    This is an automated commit created by GitHub Actions workflow,
    in the gateway repository.
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -14,7 +14,7 @@ import (
 var (
 	FabricatorVersion = meta.Version(version.Version)
 	FabricVersion     = meta.Version("v0.72.6")
-	GatewayVersion    = meta.Version("v0.3.0")
+	GatewayVersion    = meta.Version("v0.4.1")
 )
 
 var Versions = fabapi.Versions{
