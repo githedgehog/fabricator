f156ac39ba8a68c7aadf0aa35cd3678564c84f87
Sergei Lukianov <me@slukjanov.name>
2025-05-16 21:28:20 -0700

    bump: gateway to v0.7.1
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- vendor/go.githedgehog.com/gateway/api/gateway/v1alpha1/gateway_types.go
+++ vendor/go.githedgehog.com/gateway/api/gateway/v1alpha1/gateway_types.go
@@ -85,6 +85,17 @@ func (gw *Gateway) Validate(_ context.Context, _ kclient.Reader) error {
 		return fmt.Errorf("ProtocolIP %s must be an IPv4 address", gw.Spec.ProtocolIP) //nolint:goerr113
 	}
 
+	vtepIP, err := netip.ParsePrefix(gw.Spec.VTEPIP)
+	if err != nil {
+		return fmt.Errorf("invalid VTEPIP %s: %w", gw.Spec.VTEPIP, err)
+	}
+	if vtepIP.Bits() != 32 {
+		return fmt.Errorf("VTEPIP %s must be a /32 prefix", gw.Spec.VTEPIP) //nolint:goerr113
+	}
+	if !vtepIP.Addr().Is4() {
+		return fmt.Errorf("VTEPIP %s must be an IPv4 address", gw.Spec.VTEPIP) //nolint:goerr113
+	}
+
 	if gw.Spec.ASN == 0 {
 		return fmt.Errorf("ASN must be set") //nolint:goerr113
 	}
