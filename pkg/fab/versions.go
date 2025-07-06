bf0fc805f18523c3abdf95cbae5f08fd88ae058d
Sergei Lukianov <me@slukjanov.name>
2025-07-06 14:03:52 -0700

    bump: go.githedgehog.com/gateway v0.10.0 => v0.12.0
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -14,7 +14,7 @@ import (
 var (
 	FabricatorVersion = meta.Version(version.Version)
 	FabricVersion     = meta.Version("v0.82.0")
-	GatewayVersion    = meta.Version("v0.10.0")
+	GatewayVersion    = meta.Version("v0.12.0")
 )
 
 var Versions = fabapi.Versions{
