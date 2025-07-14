17606ed8db139d729e7593b97a9a7f0942989e34
Sergei Lukianov <me@slukjanov.name>
2025-07-14 15:44:58 -0700

    chore: just add helper to bump components
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -15,6 +15,8 @@ var (
 	FabricatorVersion = meta.Version(version.Version)
 	FabricVersion     = meta.Version("v0.83.0")
 	GatewayVersion    = meta.Version("v0.14.0")
+	DataplaneVersion  = meta.Version("v123")
+	FRRVersion        = meta.Version("v456")
 )
 
 var Versions = fabapi.Versions{
@@ -71,8 +73,8 @@ var Versions = fabapi.Versions{
 		API:        GatewayVersion,
 		Controller: GatewayVersion,
 		Agent:      GatewayVersion,
-		Dataplane:  "2025-07-07.main.x86_64-unknown-linux-gnu.debug.275735b8c1a1215807cae9c1358cb11a809c6831", // TODO set actual version
-		FRR:        "e04d777f4b8836eade82072ac4732e1cf386ac9d.debug",                                          // TODO set actual version
+		Dataplane:  DataplaneVersion,
+		FRR:        FRRVersion,
 	},
 	VLAB: fabapi.VLABVersions{
 		ONIE:    "v0.2.0",
