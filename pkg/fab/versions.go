5e16a06db47c89f86cbf413a4ab14f0ec17d1186
Sergei Lukianov <me@slukjanov.name>
2025-07-25 09:31:45 -0700

    bump: dp to master and frr to Fredi's pr with fixes
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -17,8 +17,8 @@ var (
 	FabricatorVersion = meta.Version(version.Version)
 	FabricVersion     = meta.Version("v0.87.3")
 	GatewayVersion    = meta.Version("v0.17.0")
-	DataplaneVersion  = meta.Version("main.x86_64-unknown-linux-gnu.debug.f27f76cd91213cf4dc85d0dab95e7c70ede30efc")
-	FRRVersion        = meta.Version("0ba323e489ea2baf3f85fc42ff23aff674a25690.debug")
+	DataplaneVersion  = meta.Version("main.x86_64-unknown-linux-gnu.debug.ee0637af132f2a0a25f22d1eb00506e8f3525861")
+	FRRVersion        = meta.Version("9028d326221de8ea57be5a10e8742f7293b395e6.debug")
 	BCMSONiCVersion   = meta.Version("v4.5.0")
 
 	// Upgrade constraints, "-0" to include pre-releases
