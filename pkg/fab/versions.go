4161e851898525bda094f0a8fb08494b2201700f
Fredi Raspall <fredi@githedgehog.com>
2025-08-11 13:16:54 +0200

    bump: dp w/ fix vrf iterator
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -17,7 +17,7 @@ var (
 	FabricatorVersion = meta.Version(version.Version)
 	FabricVersion     = meta.Version("v0.87.4")
 	GatewayVersion    = meta.Version("v0.17.0")
-	DataplaneVersion  = meta.Version("2025-08-08.dirty.pr-fredi-fix-vpc-mgr.x86_64-unknown-linux-gnu.debug.b47b048dbbee0c3642a4a247f11925d42ff949a0")
+	DataplaneVersion  = meta.Version("2025-08-11.dirty.pr-fredi-fix-vrf-iterator.x86_64-unknown-linux-gnu.debug.637c7f9f5cf9c91bc6d1ee2a0da543cadda93e1c")
 	FRRVersion        = meta.Version("7e7b0ff13cb13b49977076b091d90cfb8c526b3b.debug")
 	BCMSONiCVersion   = meta.Version("v4.5.0")
 
