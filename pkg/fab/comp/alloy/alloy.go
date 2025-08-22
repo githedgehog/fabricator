070ff1d39797fb4fc4628c88dfd378c1396a07dd
Sergei Lukianov <me@slukjanov.name>
2025-08-21 23:38:32 -0700

    chore(o11y): only deploy alloy-gw if gw is enabled
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- pkg/fab/comp/alloy/alloy.go
+++ pkg/fab/comp/alloy/alloy.go
@@ -38,89 +38,93 @@ var gwValuesTmpl string
 var _ comp.KubeInstall = Install
 
 func Install(cfg fabapi.Fabricator) ([]kclient.Object, error) {
-	controlVIP, err := cfg.Spec.Config.Control.VIP.Parse()
-	if err != nil {
-		return nil, fmt.Errorf("parsing control VIP: %w", err)
-	}
-	proxyURL := fmt.Sprintf("http://%s:%d", controlVIP.Addr().String(), controlproxy.NodePort)
+	res := []kclient.Object{}
 
-	registryURL, err := comp.RegistryURL(cfg)
-	if err != nil {
-		return nil, fmt.Errorf("getting registry URL: %w", err)
-	}
+	if cfg.Spec.Config.Gateway.Enable {
+		controlVIP, err := cfg.Spec.Config.Control.VIP.Parse()
+		if err != nil {
+			return nil, fmt.Errorf("parsing control VIP: %w", err)
+		}
+		proxyURL := fmt.Sprintf("http://%s:%d", controlVIP.Addr().String(), controlproxy.NodePort)
 
-	tolerations, err := kyaml.Marshal([]corev1.Toleration{
-		{
-			Key:      fabapi.RoleTaintKey(fabapi.NodeRoleGateway),
-			Operator: corev1.TolerationOpExists,
-			Effect:   corev1.TaintEffectNoExecute,
-		},
-	})
-	if err != nil {
-		return nil, fmt.Errorf("gateway tolerations: %w", err)
-	}
+		registryURL, err := comp.RegistryURL(cfg)
+		if err != nil {
+			return nil, fmt.Errorf("getting registry URL: %w", err)
+		}
 
-	gwAlloyCfg := alloy.Config{
-		ProxyURL:     proxyURL,
-		AutoHostname: true,
-		Targets:      cfg.Spec.Config.Observability.Targets,
+		tolerations, err := kyaml.Marshal([]corev1.Toleration{
+			{
+				Key:      fabapi.RoleTaintKey(fabapi.NodeRoleGateway),
+				Operator: corev1.TolerationOpExists,
+				Effect:   corev1.TaintEffectNoExecute,
+			},
+		})
+		if err != nil {
+			return nil, fmt.Errorf("gateway tolerations: %w", err)
+		}
 
-		Scrapes:  map[string]alloy.Scrape{},
-		LogFiles: map[string]alloy.LogFile{},
-	}
-	if cfg.Spec.Config.Gateway.Observability != nil {
-		obs := cfg.Spec.Config.Gateway.Observability
-		if obs.Dataplane.Metrics {
-			gwAlloyCfg.Scrapes["dataplane"] = alloy.Scrape{
-				Address:         net.JoinHostPort("127.0.0.1", strconv.Itoa(gateway.DataplaneMetricsPort)),
-				IntervalSeconds: obs.Dataplane.MetricsInterval,
-				Relabel:         obs.Dataplane.MetricsRelabel,
+		gwAlloyCfg := alloy.Config{
+			ProxyURL:     proxyURL,
+			AutoHostname: true,
+			Targets:      cfg.Spec.Config.Observability.Targets,
+
+			Scrapes:  map[string]alloy.Scrape{},
+			LogFiles: map[string]alloy.LogFile{},
+		}
+		if cfg.Spec.Config.Gateway.Observability != nil {
+			obs := cfg.Spec.Config.Gateway.Observability
+			if obs.Dataplane.Metrics {
+				gwAlloyCfg.Scrapes["dataplane"] = alloy.Scrape{
+					Address:         net.JoinHostPort("127.0.0.1", strconv.Itoa(gateway.DataplaneMetricsPort)),
+					IntervalSeconds: obs.Dataplane.MetricsInterval,
+					Relabel:         obs.Dataplane.MetricsRelabel,
+				}
+			}
+			if obs.FRR.Metrics {
+				gwAlloyCfg.Scrapes["frr"] = alloy.Scrape{
+					Address:         net.JoinHostPort("127.0.0.1", strconv.Itoa(gateway.FRRMetricsPort)),
+					IntervalSeconds: obs.FRR.MetricsInterval,
+					Relabel:         obs.FRR.MetricsRelabel,
+				}
+			}
+			if obs.Unix.Metrics {
+				gwAlloyCfg.Scrapes["unix"] = alloy.Scrape{
+					Unix: alloy.ScrapeUnix{
+						Enable:     true,
+						Collectors: obs.Unix.MetricsCollectors,
+					},
+					IntervalSeconds: obs.Unix.MetricsInterval,
+					Relabel:         obs.Unix.MetricsRelabel,
+				}
 			}
 		}
-		if obs.FRR.Metrics {
-			gwAlloyCfg.Scrapes["frr"] = alloy.Scrape{
-				Address:         net.JoinHostPort("127.0.0.1", strconv.Itoa(gateway.FRRMetricsPort)),
-				IntervalSeconds: obs.FRR.MetricsInterval,
-				Relabel:         obs.FRR.MetricsRelabel,
-			}
+
+		gwAlloyConfigData, err := gwAlloyCfg.Render()
+		if err != nil {
+			return nil, fmt.Errorf("gateway alloy config: %w", err)
 		}
-		if obs.Unix.Metrics {
-			gwAlloyCfg.Scrapes["unix"] = alloy.Scrape{
-				Unix: alloy.ScrapeUnix{
-					Enable:     true,
-					Collectors: obs.Unix.MetricsCollectors,
-				},
-				IntervalSeconds: obs.Unix.MetricsInterval,
-				Relabel:         obs.Unix.MetricsRelabel,
-			}
+
+		gwAlloyValues, err := tmpl.Render("values", gwValuesTmpl, map[string]any{
+			"Registry":    registryURL,
+			"Image":       comp.JoinURLParts(comp.RegPrefix, ImageRef),
+			"Version":     string(Version(cfg)),
+			"Config":      string(gwAlloyConfigData),
+			"Tolerations": string(tolerations),
+		})
+		if err != nil {
+			return nil, fmt.Errorf("gateway alloy values: %w", err)
 		}
+
+		chartVersion := string(Version(cfg))
+		gwAlloyChart, err := comp.NewHelmChart(cfg, "alloy-gw", ChartRef, chartVersion, "", false, string(gwAlloyValues))
+		if err != nil {
+			return nil, fmt.Errorf("gateway alloy chart: %w", err)
+		}
+
+		res = append(res, gwAlloyChart)
 	}
 
-	gwAlloyConfigData, err := gwAlloyCfg.Render()
-	if err != nil {
-		return nil, fmt.Errorf("gateway alloy config: %w", err)
-	}
-
-	gwAlloyValues, err := tmpl.Render("values", gwValuesTmpl, map[string]any{
-		"Registry":    registryURL,
-		"Image":       comp.JoinURLParts(comp.RegPrefix, ImageRef),
-		"Version":     string(Version(cfg)),
-		"Config":      string(gwAlloyConfigData),
-		"Tolerations": string(tolerations),
-	})
-	if err != nil {
-		return nil, fmt.Errorf("gateway alloy values: %w", err)
-	}
-
-	chartVersion := string(Version(cfg))
-	gwAlloyChart, err := comp.NewHelmChart(cfg, "alloy-gw", ChartRef, chartVersion, "", false, string(gwAlloyValues))
-	if err != nil {
-		return nil, fmt.Errorf("gateway alloy chart: %w", err)
-	}
-
-	return []kclient.Object{
-		gwAlloyChart,
-	}, nil
+	return res, nil
 }
 
 var _ comp.ListOCIArtifacts = Artifacts
