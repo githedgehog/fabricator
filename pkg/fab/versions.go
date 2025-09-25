1e1586776214665ce0a18bb631bd240a970a1b2f
Logan Blyth <logan@githedgehog.com>
2025-09-25 10:20:44 -0400

    bump: september flatcar
    Signed-off-by: Logan Blyth <logan@githedgehog.com>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -49,8 +49,8 @@ var Versions = fabapi.Versions{
 		Ctl:            FabricatorVersion,
 		NodeConfig:     FabricatorVersion,
 		Pause:          "3.6", // wait image from k3s // TODO embed wait into node-config image?
-		ControlUSBRoot: "v4230.2.1-hh3",
-		Flatcar:        "v4230.2.1",
+		ControlUSBRoot: "v4230.2.3-hh1",
+		Flatcar:        "v4230.2.3",
 	},
 	Fabric: fabapi.FabricVersions{
 		API:        FabricVersion,
@@ -92,7 +92,7 @@ var Versions = fabapi.Versions{
 	},
 	VLAB: fabapi.VLABVersions{
 		ONIE:    "v0.2.0",
-		Flatcar: "v4230.2.1",
+		Flatcar: "v4230.2.3",
 	},
 }
 
