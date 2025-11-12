c18470c3457a28716d7a70c4515d2d634af45488
Emanuele Di Pascale <emanuele@githedgehog.com>
2025-11-12 17:25:30 +0100

    chore: harden hhnet + dhcp lease check
    make sure that there is no path in which the hhnet script
    can silently exit without succeeding, and improve logging
    in the release test to ease troubleshooting
    
    Signed-off-by: Emanuele Di Pascale <emanuele@githedgehog.com>

--- pkg/hhfab/hhnet.sh
+++ pkg/hhfab/hhnet.sh
@@ -2,22 +2,22 @@
 # Copyright 2024 Hedgehog
 # SPDX-License-Identifier: Apache-2.0
 
-
 set -e
+trap 'echo "Error on line $LINENO: $BASH_COMMAND" >&2' ERR
 
 function cleanup() {
     mapfile -t bond_intfs < <(ip -brief link show type bond)
 
     for intf in "${bond_intfs[@]}"; do
             intf="${intf%% *}"
-            sudo ip link del "$intf"
+            sudo ip link del "$intf" || echo "warning: could not delete $intf" >&2
     done
 
     mapfile -t vlan_intfs < <(ip -brief link show type vlan)
     for intf in "${vlan_intfs[@]}"; do
             intf="${intf%@*}"
-            sudo ip link set "$intf" down
-            sudo ip link del "$intf"
+            sudo ip link set "$intf" down || echo "warning: could not bring down $intf" >&2
+            sudo ip link del "$intf" || echo "warning: could not delete $intf" >&2
     done
     sleep 1
 }
@@ -34,7 +34,7 @@ function setup_bond() {
 
     for iface in "${@:3}"; do
         # cannot enslave interface if it is up
-        sudo ip l s "$iface" down 2> /dev/null || true
+        sudo ip l s "$iface" down 2> /dev/null || echo "warning: could not bring down $iface" >&2
         sudo ip l s "$iface" master "$bond_name"
     done
 
