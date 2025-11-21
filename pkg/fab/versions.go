7dd10b0c7d5946dd6b8b24f6cc2b5d010eedf3db
Sergei Lukianov <me@slukjanov.name>
2025-11-21 10:05:41 -0800

    bump: kube to 1.34.2
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -30,7 +30,7 @@ var (
 
 var Versions = fabapi.Versions{
 	Platform: fabapi.PlatformVersions{
-		K3s:               "v1.34.1-k3s1",
+		K3s:               "v1.34.2-k3s1",
 		Zot:               "v2.1.9",
 		ZotChart:          "v0.1.67-hh1",
 		CertManager:       "v1.18.2",
