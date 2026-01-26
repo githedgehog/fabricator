a6753fcc5bc48810e931dbb6c433630bac39c8ee
Frostman <134872+Frostman@users.noreply.github.com>
2026-01-26 18:55:30 +0000

    bump: frr to v0.5.0
    This is an automated commit created by GitHub Actions workflow,
    in the frr repository.
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -18,7 +18,7 @@ var (
 	FabricVersion     = meta.Version("v0.103.0")
 	GatewayVersion    = meta.Version("v0.38.0")
 	DataplaneVersion  = meta.Version("v0.9.1")
-	FRRVersion        = meta.Version("v0.4.0")
+	FRRVersion        = meta.Version("v0.5.0")
 	BCMSONiCVersion   = meta.Version("v4.5.0")
 	CLSSONiCVersion   = meta.Version("v4.2.1")
 
