bd1eaeb380202f6e7f917b9732a7915759cc7ae9
Frostman <134872+Frostman@users.noreply.github.com>
2026-03-25 20:39:11 +0000

    bump: fabric to v0.115.0
    This is an automated commit created by GitHub Actions workflow,
    in the fabric repository.
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -15,7 +15,7 @@ import (
 
 var (
 	FabricatorVersion = meta.Version(version.Version)
-	FabricVersion     = meta.Version("v0.114.0")
+	FabricVersion     = meta.Version("v0.115.0")
 	DataplaneVersion  = meta.Version("v0.16.1")
 	FRRVersion        = meta.Version("v0.16.1")
 	BCMSONiCVersion   = meta.Version("v4.5.0")
