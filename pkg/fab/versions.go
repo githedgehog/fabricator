db9090b9c87e78592f73fce455d1238165659c39
Frostman <134872+Frostman@users.noreply.github.com>
2026-03-31 19:27:46 +0000

    bump: fabric to v0.115.1
    This is an automated commit created by GitHub Actions workflow,
    in the fabric repository.
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>
    (cherry picked from commit 1e63f67cad1fee4a5f002ec5a59fe388d947dc46)

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -15,7 +15,7 @@ import (
 
 var (
 	FabricatorVersion = meta.Version(version.Version)
-	FabricVersion     = meta.Version("v0.115.0")
+	FabricVersion     = meta.Version("v0.115.1")
 	DataplaneVersion  = meta.Version("v0.17.0")
 	FRRVersion        = meta.Version("v0.17.0")
 	BCMSONiCVersion   = meta.Version("v4.5.0")
