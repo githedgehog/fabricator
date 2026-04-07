116b8c59eb43d17d184c2703d63a2fcb41b3f383
Logan Blyth <logan@githedgehog.com>
2026-04-07 10:44:21 -0400

    feat: test flatcar confext
    Signed-off-by: Logan Blyth <logan@githedgehog.com>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -52,8 +52,8 @@ var Versions = fabapi.Versions{
 		Ctl:            FabricatorVersion,
 		NodeConfig:     FabricatorVersion,
 		Pause:          "3.6", // wait image from k3s // TODO embed wait into node-config image?
-		ControlUSBRoot: "v4459.2.4-hh1",
-		Flatcar:        "v4459.2.4",
+		ControlUSBRoot: "v4593.1.0-hh1",
+		Flatcar:        "v4593.1.0",
 	},
 	Fabric: fabapi.FabricVersions{
 		API:        FabricVersion,
@@ -94,7 +94,7 @@ var Versions = fabapi.Versions{
 	},
 	VLAB: fabapi.VLABVersions{
 		ONIE:    "v0.2.1",
-		Flatcar: "v4459.2.4",
+		Flatcar: "v4593.1.0",
 	},
 }
 
