d05d88ae7132fba8ef8ff58cbcc221ca38c00caf
Sergei Lukianov <me@slukjanov.name>
2026-04-13 08:47:46 -0700

    chore: make all bins smaller
    E.g. hhfab-linux-amd64/hhfab: 111 MB -> 96 MB
    
    All other bins import API so would be the same change.
    
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- vendor/github.com/openconfig/gnmic/pkg/api/types/sasl.go
+++
@@ -1,8 +0,0 @@
-package types
-
-type SASL struct {
-	User      string `mapstructure:"user,omitempty"`
-	Password  string `mapstructure:"password,omitempty"`
-	Mechanism string `mapstructure:"mechanism,omitempty"`
-	TokenURL  string `mapstructure:"token-url,omitempty"`
-}
