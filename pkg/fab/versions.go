1acb8bcf8aafe9293f6e1033711a9762b0e32534
Sergei Lukianov <me@slukjanov.name>
2026-04-14 20:49:04 -0700

    bump: alloy to v1.15.1
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -40,7 +40,7 @@ var Versions = fabapi.Versions{
 		Reloader:          "v1.4.14",
 		NTP:               "v0.0.4",
 		NTPChart:          FabricatorVersion,
-		Alloy:             "v1.13.2",
+		Alloy:             "v1.15.1",
 		ControlProxy:      "v1.11.2-hh2",
 		ControlProxyChart: FabricatorVersion,
 		BashCompletion:    "v2.16.0",
