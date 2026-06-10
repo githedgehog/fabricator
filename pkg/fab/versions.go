187beb79a471450dc25e9b4f29fc66679b46c4c7
qmonnet <17001771+qmonnet@users.noreply.github.com>
2026-06-10 21:46:10 +0000

    bump: dataplane/frr to v0.22.0
    This is an automated commit created by GitHub Actions workflow,
    in the dataplane repository.
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -23,10 +23,10 @@ const (
 	FabricVersion = meta.Version("v0.122.1")
 
 	// Gateway Dataplane version (including WASM validator)
-	DataplaneVersion = meta.Version("v0.21.0")
+	DataplaneVersion = meta.Version("v0.22.0")
 
 	// Gateway FRR version
-	FRRVersion = meta.Version("v0.21.0")
+	FRRVersion = meta.Version("v0.22.0")
 
 	// Broadcom Enterprise SONiC version (including all flavors)
 	BCMSONiCVersion = meta.Version("v4.5.0")
