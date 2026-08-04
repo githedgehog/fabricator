badb3e7b7ab9e065e76c161777618e976874c15f
Logan Blyth <logan@githedgehog.com>
2026-08-04 16:34:51 -0400

    incremental commit. still in idea phase
    Signed-off-by: Logan Blyth <logan@githedgehog.com>

--- pkg/fab/recipe/hostConfigs.go
+++ pkg/fab/recipe/hostConfigs.go
@@ -2,6 +2,7 @@ package recipe
 
 // Policy - Only add a file / setting to butane file if we need it before running the install recipe
 // The rest should be orderd in the install or upgrade recipe as needed.
+// The initial build and upgrade will need to pick values from the config for templating
 
 // sshd drop in
 // nftables
@@ -15,6 +16,9 @@ type unitFile struct {
 	pritority  string // 10,20,30-filename.network
 }
 
+// dummy interface
+// mgmt interface
+// ext interface
 type networkFile struct {
 	tmpl       string
 	tmplValues map[string]string
@@ -44,9 +48,14 @@ type settingsFile struct {
 	dstPath      string
 }
 
-type ConfigAction interface {
+// this interface is what lets me do all the changes on diverse sets of interfaces
+type Configuration interface {
 	enforce() (bool, error)
 	render() (string, error)
 	activate() error
 	deactivate() error
 }
+
+// TODO make the settingsFile, cmdSettings, networkFile, unitFil  into a single slice or similar so I can iterate through the slice and call enforce on them
+// TODO
+// TODO things inside the struct will need to be templated, ho
