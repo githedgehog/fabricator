97245731e91469a33097e0f02e72523dd30daadc
Logan Blyth <logan@githedgehog.com>
2026-08-04 11:26:01 -0400

    incremental commint with notes
    Signed-off-by: Logan Blyth <logan@githedgehog.com>

--- pkg/fab/recipe/hostConfigs.go
+++ pkg/fab/recipe/hostConfigs.go
@@ -1,16 +1,51 @@
 package recipe
 
-type hostConfig struct {
+// Policy - Only add a file / setting to butane file if we need it before running the install recipe
+// The rest should be orderd in the install or upgrade recipe as needed.
+
+// sshd drop in
+// nftables
+type unitFile struct {
 	tmpl       string
 	tmplValues map[string]string
 	startCmd   string
 	stopCmd    string
-	verifyCmd  string
+	verifyCmd  string // systemd-analyze verify
 	dstPath    string
+	pritority  string // 10,20,30-filename.network
+}
+
+type networkFile struct {
+	tmpl       string
+	tmplValues map[string]string
+	startCmd   string
+	stopCmd    string
+	verifyCmd  string // systemd-analyze verify
+	dstPath    string
+	pritority  string // 10,20,30-filename.network
+}
+
+// hostnamectl
+// timedatectl
+// iptables vxlan command
+type cmdSetting struct {
+	binPath   string
+	args      string
+	startCmd  string
+	stopCmd   string
+	verifyCmd string
+	dstPath   string
+}
+
+// sysctl file
+// modules.d file
+type settingsFile struct {
+	fileContents string
+	dstPath      string
 }
 
 type ConfigAction interface {
-	enforce() error
+	enforce() (bool, error)
 	render() (string, error)
 	activate() error
 	deactivate() error
