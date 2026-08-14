a673c7456673561c9301db56f6f1b923e110b4db
Logan Blyth <logan@githedgehog.com>
2026-08-14 16:57:05 -0400

    fix: call enforceNICs on controller
    Signed-off-by: Logan Blyth <logan@githedgehog.com>

--- pkg/fab/recipe/control_upgrade.go
+++ pkg/fab/recipe/control_upgrade.go
@@ -179,6 +179,9 @@ func (c *ControlUpgrade) Run(ctx context.Context) error {
 	if err := c.setupFirewall(ctx); err != nil {
 		return fmt.Errorf("setup firewall: %w", err)
 	}
+	if err := enforceNICNames(ctx, c.WorkDir, c.Control.Spec.Management.Interface, c.Control.Spec.External.Interface); err != nil {
+		return fmt.Errorf("enforcing nic names: %w", err)
+	}
 
 	if err := upgradeFlatcar(ctx, string(flatcar.Version(c.Fab)), c.Yes); err != nil {
 		return fmt.Errorf("upgrading Flatcar: %w", err)
