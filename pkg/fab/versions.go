c4d9ba71c951309d381fe8e110560c797997ab3e
Frostman <134872+Frostman@users.noreply.github.com>
2026-08-14 23:52:38 +0000

    bump: fabric to v0.129.4
    This is an automated commit created by GitHub Actions workflow,
    in the fabric repository.
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkg/fab/versions.go
+++ pkg/fab/versions.go
@@ -20,7 +20,7 @@ const (
 	Release = "26.03.0"
 
 	// Fabric version used for all fabric components
-	FabricVersion = meta.Version("v0.129.3")
+	FabricVersion = meta.Version("v0.129.4")
 
 	// Gateway Dataplane version (including WASM validator)
 	DataplaneVersion = meta.Version("v0.25.2")
